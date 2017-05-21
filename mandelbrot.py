37ea8d741e10d1c9ccaaacd8b7373914a087e15b
Corey Goldberg <cgoldberg@gmail.com>
2017-05-21 10:20:10 -0400

    py3 updates

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 
 
-"""compute and render mandelbrot set"""
+"""Compute and render Mandelbrot set.
 
+updates: Corey Goldberg (2012)
+original: Kirby Urner (2004)
 
-# updated: Corey Goldberg (2012)
-# original program: Kirby Urner (2004) - http://www.4dsolutions.net/ocn/fractals.html
+"""
 
 
 import time
 
-import Image  # PIL
-import ImagePalette
-
-
-
-PATH = './'  # path for saving files
-
+from PIL import Image, ImagePalette
 
 
 class Mandelbrot(object):
-    
-    def __init__(self, filename, size, n=64, box=((-2.0, 1.25), (0.5, -1.25))):
+
+    def __init__(self, outfile, size, n=64, box=((-2.0, 1.25), (0.5, -1.25))):
         self.size = size
-        self.filename = filename
+        self.outfile = outfile
         self.n = n
         self.uleft  = box[0]
         self.lright = box[1]
@@ -34,14 +29,14 @@ class Mandelbrot(object):
     def __new_image(self):
         image = Image.new('P', self.size)
         palette = [0, 0, 0]
-        for i in xrange(0, 255):
-            palette.extend((i*5%200 + 55, i*7%200 + 55, i*11%200 + 55))
+        for i in range(0, 255):
+            palette.extend((i*5%200+55, i*7%200+55, i*11%200+55))
         image.putpalette(palette)
         return image
-        
+
     def __save_image(self):
-        self.image.save(self.filename)
-        print 'saved: %r' % self.filename
+        self.image.save(self.outfile)
+        print('saved: {}'.format(self.outfile))
 
     def __get_coords(self, x, y):
         percentx = x / float(self.size[0])
@@ -49,32 +44,34 @@ class Mandelbrot(object):
         xp = self.uleft[0] + percentx * (self.xwidth)
         yp = self.uleft[1] - percenty * (self.ywidth)
         return (xp, yp)
-    
+
     def __fractal(self, x, y):
         z = complex(x, y)
         o = complex(0, 0)
         dotcolor = 0  # default, convergent
-        for trials in xrange(self.n):
+        for trials in range(self.n):
             if abs(o) <= 2.0:
                 o = o**2 + z
             else:
                 dotcolor = trials
                 break  # diverged
-        return dotcolor 
-    
+        return dotcolor
+
     def compute(self):
-        print 'computing %s...' % self.__class__.__name__
+        print('computing {}...'.format(self.__class__.__name__))
         start = time.time()
         self.image = self.__new_image()
-        for x in xrange(self.size[0]):
-            for y in xrange(self.size[1]):
-                xp, yp = self.__get_coords(x, y)                
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                xp, yp = self.__get_coords(x, y)
                 dotcolor = self.__fractal(xp, yp)
                 self.image.putpixel((x, y), dotcolor)
-        print 'compute time: %.2f secs' % (time.time() - start)
         self.__save_image()
+        end = time.time()
+        print('compute time: {} secs'.format(end - start))
 
-    
+
 if __name__ == '__main__':
-    m = Mandelbrot(PATH + 'mandelbrot.png', (500, 500))
-    m.compute()
+    outfile ='./mandelbrot.png'
+    dimensions = (500, 500)
+    Mandelbrot(outfile, dimensions).compute()
